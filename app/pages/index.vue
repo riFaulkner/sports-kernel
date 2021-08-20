df370a1e3f4b14838953d1b94c7ac9a189dd69cd
Rick <Richard.faulkner93@gmail.com>
2021-08-20 14:19:55 -0600

    Fixing typo, removing ts config file

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,19 +4,19 @@
       <v-card class="logo py-4 d-flex justify-center" />
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Sports-kernel
+          Welcome to Sports-kernel
         </v-card-title>
         <v-card-text>
-          Look around and explore the application!
+          Look around and explore the application
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/leauge-home"
+            to="/league-home"
           >
-            Continue
+            Continue to League
           </v-btn>
         </v-card-actions>
       </v-card>
