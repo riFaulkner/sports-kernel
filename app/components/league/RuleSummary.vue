3b37840c20b8ecd8991658766ac54de9d2e8d5ec
Kyle Brogan <BROGAKA@GMAIL.COM>
2022-05-13 03:56:59 +0000

    rule summary testing

--- app/components/league/RuleSummary.vue
+++ app/components/league/RuleSummary.vue
@@ -1,17 +1,39 @@
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel
-      v-for="(item,i) in 5"
-      :key="i"
+  <div>
+    <div class="d-flex">
+      <br>
+      <p>Below are the proposed rules for a best ball, contract, dynasty fantasy football league. <br>
+      The intent is to create a league that is low/no maintenance during the season, but still allows for lots of fun in the off season.</p>
+    </div>
+
+    <v-expansion-panels
+      v-model="panel"
+      :disabled="disabled"
+      multiple
     >
-      <v-expansion-panel-header>
-        Item
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Core Elements (What the hell are we doing here?)</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <h3>Best Ball</h3>
+          <p>"Your best performing players are automatically "started". You make no start-sit decisions and there is no weekly waiver wire. The roster is a bit bigger as a result to account for injuries and bye weeks. Most standard best ball leagues don't allow trading (they are just draft exercises), but this league will have trading. Matchups are still head-to-head each week."</p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header>Panel 2</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-header>Panel 3</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script>
