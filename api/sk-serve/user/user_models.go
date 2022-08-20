a0cafe3ffd95d5d4312194e1a801cfb7b1abbac4
Rick <Richard.faulkner93@gmail.com>
2022-08-20 14:48:14 -0600

    Updating to use a league snippet instead of expecting an entire league

--- api/sk-serve/user/user_models.go
+++ api/sk-serve/user/user_models.go
@@ -1,13 +1,11 @@
 package user
 
-import "github.com/rifaulkner/sports-kernel/api/sk-serve/league"
-
 type UserPreferences struct {
-	ID                string           `json:"id"`
-	OwnerName         string           `json:"ownerName"`
-	PreferredLeagueID *string          `json:"preferredLeagueId"`
-	IsAdmin           *bool            `json:"isAdmin"`
-	Leagues           []*league.League `json:"leagues"`
+	ID                string                          `json:"id"`
+	OwnerName         string                          `json:"ownerName"`
+	PreferredLeagueID *string                         `json:"preferredLeagueId"`
+	IsAdmin           *bool                           `json:"isAdmin"`
+	Leagues           []*UserPreferencesLeagueSnippet `json:"leagues"`
 }
 
 type UserPreferencesLeagueSnippet struct {
