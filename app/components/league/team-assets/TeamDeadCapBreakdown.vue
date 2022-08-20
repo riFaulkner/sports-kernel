a38d07c50113cd0aafc0b87d72e08d682f596873
Rick <Richard.faulkner93@gmail.com>
2022-08-20 16:18:34 -0600

    FE update to more safely handle when a team has no deadcap

--- app/components/league/team-assets/TeamDeadCapBreakdown.vue
+++ app/components/league/team-assets/TeamDeadCapBreakdown.vue
@@ -35,7 +35,7 @@ export default {
   },
   computed: {
     processedDeadCap() {
-      if (this.deadCap?.length === 0) {
+      if (this.deadCap === null || this.deadCap.length === 0) {
         return []
       }
       let tableView = []
@@ -64,7 +64,10 @@ export default {
       return tableView
     },
     headers() {
-      const nextDeadCapYear = this.deadCap[0].year
+      const nextDeadCapYear = new Date().getFullYear()
+      if (this.deadCap?.length > 0) {
+        this.deadCap[0].year
+      }
 
       return [
         {text: "Player", value: "name", align: "center"},
